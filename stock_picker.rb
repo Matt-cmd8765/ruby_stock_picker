b310014fa69a993a9982712320fa863a36c8f923
Matt Marini <mattmarini8765@gmail.com>
2023-04-26 17:51:03 +1000

    Completed project

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,17 +1,25 @@
 stock_prices = [17,3,6,9,15,8,6,1,10]
 
-hash = {}
-
- stock_prices.each do |buy_price|
-    the_buy_index = stock_prices.index(buy_price)    
-     stock_prices.each.with_index(the_buy_index) do |sell_price, sell_idx|
-        days_array = []
-        buy_index = stock_prices.index(buy_price)
-        days_array.push(buy_index)
-        sell_index = stock_prices.index(sell_price)
-        days_array.push(sell_index)
-        result = sell_price - buy_price
-        hash[days_array] = result
-     end
+def stock_picker (array)
+    #Empty hash to store buy / sell results
+    hash = {}
+    #Nested arrays, returning prices and indexes
+    array.each_with_index { |buy_price, buy_index| 
+        array.each_with_index { |sell_price, sell_index|
+            #array stores the buy / sell dates to be stored as a key in the hash
+            days_array = []
+            #only add to array and hash if sell_index is greater or equal to buy index
+            if sell_index >= buy_index
+                days_array.push(buy_index)
+                days_array.push(sell_index)
+                result = sell_price - buy_price
+                hash[days_array] = result
+            end
+        }
+    }
+    #return max value from the hash
+    max_key = hash.max_by { |k,v| v}[0]
+    p "Shoulda bought and sold on these days: #{max_key}"
 end
-p hash
+
+stock_picker(stock_prices)
